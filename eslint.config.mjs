91493636dd36c3cc294458076ae8f9d9a31f7423
Dane Kolbaba <your.email@example.com>
2025-08-12 13:38:03 -0700

    more

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -9,8 +9,27 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
+
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import pluginReact from 'eslint-plugin-react';
+
 const eslintConfig = [
+  { files: ['**/*.{js,mjs,cjs,ts,tsx}'] },
+  { languageOptions: { globals: globals.browser } },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  pluginReact.configs.flat.recommended,
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    rules: {
+      '@typescript-eslint/no-require-imports': 'off',
+      '@typescript-eslint/no-unused-expressions': 'warn',
+      '@typescript-eslint/no-unused-vars': 'warn',
+    },
+    ignores: ['.next/**'],
+  },
 ];
 
 export default eslintConfig;
