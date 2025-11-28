77f511d78b2975ed86df1de921434af78ffe81f2
Dane Kolbaba <partybusexperts@gmail.com>
2025-11-28 05:02:53 -0700

    adsf

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,12 +1,18 @@
 import HeroHeaderServer from "../../components/HeroHeaderServer";
 import BlogClient from "./BlogClient";
 import { getHeroFallback } from "../../data/heroFallbacks";
+import GlobalReviewStripServer from "@/components/reviews/GlobalReviewStripServer";
+import BlogFaqSection from "@/components/blog/BlogFaqSection";
 
 export default function BlogPage() {
   return (
     <main className="min-h-screen bg-gradient-to-br from-blue-950 via-blue-900 to-black text-white">
       <HeroHeaderServer pageSlug="blog" fallback={getHeroFallback("blog")} />
       <BlogClient />
+      <div className="mt-16 space-y-16">
+        <GlobalReviewStripServer />
+        <BlogFaqSection />
+      </div>
     </main>
   );
 }
