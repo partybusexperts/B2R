98e98ee642b0facf3115ca867c828419b2c7dfb3
Gabriel Marcano <gabrielmarcano120@gmail.com>
2025-12-19 10:08:42 -0400

    stop checking cookies for public data (all)

--- lib/supabase/server.ts
+++ lib/supabase/server.ts
@@ -1,6 +1,6 @@
 import { Database } from "@/types/database.types";
 import { createServerClient } from "@supabase/ssr";
-import { cookies } from "next/headers";
+// import { cookies } from "next/headers";
 
 /**
  * Especially important if using Fluid compute: Don't put this client in a
@@ -8,27 +8,31 @@ import { cookies } from "next/headers";
  * it.
  */
 export async function createClient() {
-  const cookieStore = await cookies();
+  // const cookieStore = await cookies();
 
   return createServerClient<Database>(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY!,
     {
       cookies: {
+        // getAll() {
+        //   return cookieStore.getAll();
+        // },
+        // setAll(cookiesToSet) {
+        //   try {
+        //     cookiesToSet.forEach(({ name, value, options }) =>
+        //       cookieStore.set(name, value, options),
+        //     );
+        //   } catch {
+        //     // The `setAll` method was called from a Server Component.
+        //     // This can be ignored if you have proxy refreshing
+        //     // user sessions.
+        //   }
+        // },
         getAll() {
-          return cookieStore.getAll();
+          return [];
         },
-        setAll(cookiesToSet) {
-          try {
-            cookiesToSet.forEach(({ name, value, options }) =>
-              cookieStore.set(name, value, options),
-            );
-          } catch {
-            // The `setAll` method was called from a Server Component.
-            // This can be ignored if you have proxy refreshing
-            // user sessions.
-          }
-        },
+        setAll() {},
       },
     },
   );
