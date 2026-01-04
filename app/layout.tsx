08eebc90f234c01b4c727881f57fd98b1a3bf43f
partybusexperts <partybusexperts@gmail.com>
2026-01-03 18:21:47 -0700

    asdf

--- app/layout.tsx
+++ app/layout.tsx
@@ -29,6 +29,21 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
+      <head>
+        {/* Google tag (gtag.js) */}
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-90TGBE5355"
+        ></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-90TGBE5355');`,
+          }}
+        />
+      </head>
       <body
         className={`${geistSans.className} antialiased`}
         suppressHydrationWarning={true} // to avoid hydration mismatch warnings
